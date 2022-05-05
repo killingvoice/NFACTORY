df274ec0fb40f529d02b1e37bd640a23239d25e3
killingvoice <kyung2687@gmail.com>
2022-05-05 23:47:11 +0900

    router

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* Property add */
+router.get('/property', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* item add */
+router.get('/item', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* Make random images */
+router.get('/makeimages', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* View NFT images */
+router.get('/view', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 module.exports = router;
